9302d98e0de093a7b8a5e8b6f4bcf74366dc4943
FurqanRahim <ffurqanrahim@gmail.com>
2025-07-03 15:57:24 +0300

    Update

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -4,14 +4,16 @@ import dotenv from "dotenv";
 dotenv.config('./.env')
 import connectionDB from "./src/config/mongodb.config.js";
 import router from "./src/routes/url.routes.js";
+import cors from "cors";
 
 
 const PORT = 5000
 const app = express()
 
-
-
-
+app.use(cors({
+  origin: true, // Reflects the request origin
+  credentials: true // If you need cookies/auth
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
